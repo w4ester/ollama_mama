3c8f08f8ee5e6bf7d2d49665b93484d0c2491c06
Blake Mizerany <blake.mizerany@gmail.com>
2024-03-20 07:47:51 -0700

    debug

--- quants/quants.go
+++ quants/quants.go
@@ -61,6 +61,7 @@ func Convert(ctx context.Context, workDir, in, level string) (out string, err er
 		out,
 		level,
 	)
+	println(cmd.String())
 	b, err := cmd.CombinedOutput()
 	if err != nil {
 		// TODO(bmizerany): temp hack; better error handling / messages
